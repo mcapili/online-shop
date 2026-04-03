039a796fb2b77499a7a9d6f9a105d464957d7862
20220255 <118017744+20220255@users.noreply.github.com>
2026-04-03 20:51:20 +0800

    1. Removed Sorting by price. 2. Added favorites button and favorites action

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -4,8 +4,9 @@ import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { LuTrash2 } from "react-icons/lu";
-import { FaRegEdit } from "react-icons/fa";
+import { FaHeart, FaRegEdit, FaRegHeart } from "react-icons/fa";
 import { forwardRef } from "react";
+import { SignInButton } from "@clerk/nextjs";
 
 type btnSize = 'default' | 'lg' | 'sm';
 
@@ -78,3 +79,39 @@ export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
   }
 );
 IconButton.displayName = "IconButton";
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode='modal'>
+      <Button
+        type='button'
+        size='icon'
+        variant='outline'
+        className='p-2 cursor-pointer'
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
+  );
+};
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type='submit'
+      size='icon'
+      variant='outline'
+      className=' p-2 cursor-pointer'
+    >
+      {pending ? (
+        <ReloadIcon className=' animate-spin' />
+      ) : isFavorite ? (
+        <FaHeart />
+      ) : (
+        <FaRegHeart />
+      )}
+    </Button>
+  );
+};
--- utils/actions.ts
+++ utils/actions.ts
@@ -182,3 +182,46 @@ export const updateProductAction = async (
     return renderError(error);
   }
 };
+
+export const fetchFavoriteId = async ({ productId }: { productId: string }) => {
+  const user = await getAuthUser();
+  const favorite = await db.favorite.findFirst({
+    where: {
+      productId,
+      clerkId: user.id,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return favorite?.id || null;
+};
+
+export const toggleFavoriteAction = async (prevState: {
+  productId: string;
+  favoriteId: string | null;
+  pathname: string;
+}) => {
+  const user = await getAuthUser();
+  const { productId, favoriteId, pathname } = prevState;
+  try {
+    if (favoriteId) {
+      await db.favorite.delete({
+        where: {
+          id: favoriteId,
+        },
+      });
+    } else {
+      await db.favorite.create({
+        data: {
+          productId,
+          clerkId: user.id,
+        },
+      });
+    }
+    revalidatePath(pathname);
+    return { message: favoriteId ? 'Removed from Faves' : 'Added to Faves' };
+  } catch (error) {
+    return renderError(error);
+  }
+};
