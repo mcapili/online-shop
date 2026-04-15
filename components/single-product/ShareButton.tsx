e5f328cda1cd07aa5db3803f357a925dc54889de
20220255 <118017744+20220255@users.noreply.github.com>
2026-04-15 19:49:00 +0800

    Added react-share component and functionality

---
+++ components/single-product/ShareButton.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { LuShare2 } from "react-icons/lu";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import {
+    XShareButton,
+    LinkedinShareButton,
+    LinkedinIcon,
+    EmailShareButton,
+    EmailIcon,
+    FacebookShareButton,
+    FacebookIcon,
+    XIcon,
+} from "react-share";
+
+function ShareButton({ productId, name }: { productId: string; name: string }) {
+    const url = process.env.NEXT_PUBLIC_WEBSITE_URL;
+    const shareLink = `${url}/products/${productId}`;
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant='outline' size='icon' className='p-2'>
+                    <LuShare2 />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent
+                side='top'
+                align='end'
+                sideOffset={10}
+                className='flex items-center gap-x-2 justify-center w-full'
+            >
+                <XShareButton url={shareLink} title={name}>
+                    <XIcon size={32} round />
+                </XShareButton>
+                <FacebookShareButton url={shareLink} title={name}>
+                    <FacebookIcon size={32} round />
+                </FacebookShareButton>
+                <LinkedinShareButton url={shareLink} title={name}>
+                    <LinkedinIcon size={32} round />
+                </LinkedinShareButton>
+                <EmailShareButton url={shareLink} subject={name}>
+                    <EmailIcon size={32} round />
+                </EmailShareButton>
+            </PopoverContent>
+        </Popover>
+    );
+}
+export default ShareButton;
