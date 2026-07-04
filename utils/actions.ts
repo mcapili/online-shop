49a755d02429464a4c729d32f6d9cdca89c9e6cf
20220255 <118017744+20220255@users.noreply.github.com>
2026-07-04 19:54:30 +0800

    Get total and average review per products

--- utils/actions.ts
+++ utils/actions.ts
@@ -277,4 +277,23 @@ export const fetchProductReviews = async (productId: string) => {
 export const fetchProductReviewsByUser = async () => {};
 export const deleteReviewAction = async () => {};
 export const findExistingReview = async () => {};
-export const fetchProductRating = async () => {};
+export const fetchProductRating = async (productId: string) => {
+  const result = await db.review.groupBy({
+    by: ['productId'],
+    _avg: {
+      rating: true,
+    },
+    _count: {
+      rating: true,
+    },
+    where: {
+      productId,
+    },
+  });
+
+  // empty array if no reviews
+  return {
+    rating: result[0]?._avg.rating?.toFixed(1) ?? 0,
+    count: result[0]?._count.rating ?? 0,
+  };
+};
